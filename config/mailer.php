84af973d26072d639af19ef841e729ae5c09ed05
Jomon Johnson <jomon.entero@gmail.com>
2019-11-21 16:49:18 +0530

    init

--- config/mailer.php
+++ config/mailer.php
@@ -1,7 +1,12 @@
 <?php
 
+use codexten\yii\mailjet\AwsMailer;
+
 return [
     'awsMailer' => [
-//        'class'=>
+        'class' => AwsMailer::class,
+        'access_key' => $params['aws.key'],
+        'secret_key' => $params['aws.secret'],
+//        'host' => 'email.us-east-1.amazonaws.com' // not required
     ]
 ];
